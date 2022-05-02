c28879d685f2689385c13eafd32caafec6c91dec
Alvin Huang <17609145+alvin-huang@users.noreply.github.com>
2022-05-02 11:30:26 -0400

    use local git config

--- action-entrypoint.sh
+++ action-entrypoint.sh
@@ -11,6 +11,8 @@ pwd
 ls -la
 git config -l
 git config -l --show-origin
+git config -l --local
+git config --get --local remote.origin.url
 
 FOSSA_PROJECT=$(git config --get remote.origin.url)
 FOSSA_REVISION=$(git tag --points-at HEAD | cat)
